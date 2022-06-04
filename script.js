73b95e3ce738e4d68d7aa0fa2187759a885619a0
Phil Branch <philbranch243@gmail.com>
2022-06-04 13:08:34 -0500

    got a for loop startted

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ var stringLower = "abcdefghijklmnopqrstuvwxyz";
 var stringNum = "0123456789";
 var stringSymbol = "!@#$%^&*()";
 var charList = "";
-
+var length = 0;
 
 
 // my plan is to get a password length from the user, then ask a series of questions about what type of characters to use, 
 // I will then concatenate the corresponding strings into variable charlist and generate random numbers to choose characters from charList
 
 function generatePassword() {
-  var length = window.prompt("How long is your password? Enter a number from 8 - 128")
+  length = window.prompt("How long is your password? Enter a number from 8 - 128")
   console.log(length);
   if (length > 7 && length < 129) {
-generateCharList();
+    // I moved all my if/else to generate a charlist into the following function
+    generateCharList();
   }
   else if (length < 8 || length > 128) {
     window.alert("Sorry, but you must choose a length between 8 and 128.  Feel free to try again!");
   }
+
   charList = "";
 }
 // Get references to the #generate element
@@ -33,7 +35,7 @@ function writePassword() {
 
 }
 
-function generateCharList(){
+function generateCharList() {
   var choice = window.prompt("Do you want to include capitol letters? (Y/N)");
   if (choice === "Y" || choice === "y") {
     console.log("woohoo!");
@@ -84,6 +86,16 @@ function generateCharList(){
     choice = "";
     console.log(charList);
   }
+  //start writing code to change charList into an array
+  let arrChoice = charList.split("");
+  console.log(arrChoice);
+  //this seems to work.  now to make a loop that will choose randomly from the array and give us a password
+  for (i=1; i<length; i++) {
+    var piece = Math.floor(Math.random() * arrChoice.length);
+    console.log(piece);
+  };
+
+
   //I see in the console log that the charList is retaining value each time I run through the code so far, so I'm going to set it back to null after the function
 }
 
